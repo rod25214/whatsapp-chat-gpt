ca325ad49d815e6802156c6d7a81843509b7ccaf
rod25214 <162049110+rod25214@users.noreply.github.com>
2026-01-12 21:17:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-import qrcode from "qrcode-terminal";
-import pkg from "whatsapp-web.js";
-const { Client, LocalAuth } = pkg;
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 
 const client = new Client({
-  authStrategy: new LocalAuth(),
-  puppeteer: {
-    executablePath: "/usr/bin/chromium",
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-gpu"
-    ]
-  }
+  authStrategy: new LocalAuth()
 });
 
-client.on("qr", qr => qrcode.generate(qr, { small: true }));
-client.on("ready", () => console.log("✅ WhatsApp conectado!"));
+client.on("qr", qr => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("✅ WhatsApp conectado!");
+});
+
 client.initialize();
